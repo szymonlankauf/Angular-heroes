7c1069e20777a080c20d943bec9dc98d2c9f2a33
tesla369 <tesla369@gmail.com>
2019-04-04 14:57:20 +0200

    Added routing functionality

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -10,8 +10,6 @@ import { HeroService } from '../hero.service'
 export class HeroesComponent implements OnInit {
         
     heroes: Hero[];
-
-    selectedHero: Hero;
    
     getHeroes(): void {
         this.heroService.getHeroes().subscribe(heroes => this.heroes = heroes)
@@ -25,9 +23,6 @@ export class HeroesComponent implements OnInit {
 
     ngOnInit() {
         this.getHeroes()
-    }
-    onSelect(hero: Hero): void {
-        this.selectedHero = hero;
     }
 
 }
